cb5720f0bf9cc9891f6f340d6648c94960100013
Wafick <wafickachiambi@gmail.com>
2025-05-03 19:35:25 -0600

    Update

--- test.py
+++ test.py
@@ -61,7 +61,7 @@ class network(nn.Module):
     
 network()(torch.rand((1,5,150,150),dtype=torch.float))
 model = network()
-model.load_state_dict(torch.load("./mario200",map_location=device),strict=False)
+model.load_state_dict(torch.load("./data/2_mario200",map_location=device),strict=False)
 
 
 if __name__ == "__main__":
