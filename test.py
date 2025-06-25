6f472a2cbf22c2de09434fb16b576398095c0ffb
Wafick <wafickachiambi@gmail.com>
2025-06-25 15:51:16 -0400

    Update

--- test.py
+++ test.py
@@ -71,7 +71,7 @@ class test:
         if start:
             with torch.no_grad():
                 model = network()
-                chk = torch.load(".\mario380",map_location="cpu")
+                chk = torch.load(".\mario160",map_location="cpu")
                 model.load_state_dict(chk["model_state"],strict=False)
                 env = __class__.make_env()
                 done = True
