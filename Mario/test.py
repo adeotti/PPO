2c97c95e33fdf8dbba2f375ccbd58945b0181a42
Wafick <wafickachiambi@gmail.com>
2025-07-15 02:01:53 -0400

    Adapt test code to atch the training,fixed bugggggssss...

--- Mario/test.py
+++ Mario/test.py
@@ -1,53 +1,39 @@
-import torch,gym
+import torch,gym,sys
 import gym_super_mario_bros
 from nes_py.wrappers import JoypadSpace
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
 from gym.wrappers import GrayScaleObservation,FrameStack,ResizeObservation
 import numpy as np
-
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.distributions import Categorical
-
-device = "cpu"
 
 class network(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.LazyConv2d(32,1,1,0)
-        self.conv2 = nn.LazyConv2d(32,3,2,2)
-        self.conv3 = nn.LazyConv2d(32,3,2,2)
-        self.conv4 = nn.LazyConv2d(32,3,2,2)
-        self.output = nn.LazyLinear(80)
-
+        self.conv1 = nn.LazyConv2d(32,3,2,1)
+        self.conv2 = nn.LazyConv2d(32,3,2,1)
+        self.conv3 = nn.LazyConv2d(32,3,2,1)
+        self.conv4 = nn.LazyConv2d(32,3,2,1)
+        self.output = nn.LazyLinear(512)
         self.policy_head = nn.LazyLinear(7)
         self.value_head = nn.LazyLinear(1)
-        #self.optim = torch.optim.Adam(self.parameters(),lr=configs.lr)
         
     def forward(self,x):
         x = F.relu(self.conv1(x))
         x = F.relu(self.conv2(x))
         x = F.relu(self.conv3(x))
         x = F.relu(self.conv4(x))
-        x = torch.flatten(x,start_dim=1) # -> torch.Size([32, 169])
+        x = torch.flatten(x,start_dim=1)  
         x = F.relu(self.output(x))
         policy_output = self.policy_head(x)
-        value_output = self.value_head(x)
-        return F.softmax(policy_output,-1),value_output
+        return F.softmax(policy_output,-1)
 
-def init_weights(layer):
-    if isinstance(layer,(nn.Conv2d,nn.Linear)):
-        nn.init.orthogonal_(layer.weight)
-        nn.init.constant_(layer.bias,0.0)
-     
-model = network().to(device)
-model.forward(
-    torch.rand(
-        (1,4,100,100),dtype=torch.float32,device=device
-    )
-)
-model.apply(init_weights)
+model = network().to("cpu")
+model(torch.rand((1,4,100,100),dtype=torch.float32,device="cpu"))
 model = nn.DataParallel(model)
+chk = torch.load(".\Mario\\training_data\\1\mario380",map_location="cpu")
+model.load_state_dict(chk["model_state"],strict=True)
+model.eval()
 
 class test:
     @staticmethod
@@ -80,29 +66,20 @@ class test:
         return x
 
     @staticmethod
+    @torch.no_grad()
     def run(start,num_game):
         if start:
-            with torch.no_grad():
-                model = network()
-                chk = torch.load(".\\training_data\mario200",map_location="cpu")
-                model.load_state_dict(chk["model_state"],strict=False)
-                env = __class__.make_env()
-                done = True
-                epi_rewards = 0
-                for _ in range(num_game):
-                    if done:
-                        state,_ = env.reset()
-                        epi_rewards = 0
-    
-                    state = torch.from_numpy(np.array(state)).permute(-1,0,1,2).to(device,torch.float32) / 255.
-                    dist,_ = model(state)
-                    action = dist.argmax().item()
-                    state, reward, done, _,_ = env.step(action)
-                    epi_rewards += reward
-                    env.render()
-                env.close()
+            env = __class__.make_env()
+            done = True
+            for _ in range(num_game):
+                if done:
+                    state,_ = env.reset()
+                state = torch.from_numpy(np.array(state)).squeeze(-1).unsqueeze(0).to(torch.float32) / 255.
+                dist = model(state)
+                action = dist.argmax().item()
+                state, reward, done, _,_ = env.step(action)
+                env.render()
+            env.close()
 
-
-if __name__ == "__main__":
-    test.run(start=True,num_game=10_000)
+test.run(start=True,num_game=10_000)
      
